a1096639fd4ca96980b164b8bae04d1cc8e9c1e7
Tobi Anifowose <tobexkee@gmail.com>
2022-10-14 21:57:53 +0100

    Add basic messagebus class

---
+++ src/MessageBus.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tobexkee\Phpmessagebus;
+
+class MessageBus
+{
+    private array $middlewares = [];
+
+    public function __construct(array $middlewares = [])
+    {
+        foreach ($middlewares as $middleware) {
+            $this->addMiddleware($middleware);
+        }
+    }
+
+    public function addMiddleware(object $middleware): void
+    {
+        $this->middlewares[] = $middleware;
+    }
+
+    public function getMiddlewares(): array
+    {
+        return $this->middlewares;
+    }
+
+    public function handle(object $message): void
+    {
+        call_user_func($this->prepareNextMiddleware(0), $message);
+    }
+
+    private function prepareNextMiddleware(int $position): callable
+    {
+        if (! isset($this->middlewares[$position])) {
+            return function () {
+            };
+        }
+
+        $middleware = $this->middlewares[$position];
+
+        return function ($message) use ($middleware, $position) {
+            $middleware->handle($message, $this->prepareNextMiddleware($position + 1));
+        };
+    }
+}
